95d6b61c4a9e330bae4a2dd14d77fcd7ab32ceb9
abadanaf <abadanafchala@gmail.com>
2024-12-04 19:01:26 +0300

    the achievements section added

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,64 @@
+
+import React from 'react'
+import './About.css'
+import logotwo from '../../assets/logo (2).png'
+import myprofile2 from '../../assets/mycontact.jpg'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+            <h1>About me</h1>
+            <img src={logotwo} alt="my logo" />
+        </div>
+        <div className="about-sections">
+           <div className="about-left">
+            <img src={myprofile2} alt="my profile" />
+            </div> 
+            <div className="about-right">
+                <div className="about-para">
+                    <p>I am experienced Frontend Developer with over three years</p>
+                    <p>and also, I have a passion on Graphics Design</p>
+                </div>
+                <div className="about-skills">
+                    <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}}/></div>
+                    <div className="about-skill"><p>REACT JS</p><hr style={{width:"90%"}}/></div>
+                    <div className="about-skill"><p>BOOTSTRAP</p><hr style={{width:"60%"}}/></div>
+                    <div className="about-skill"><p>PHP</p><hr style={{width:"80%"}}/></div>
+                    <div className="about-skill"><p>LARAVEL</p><hr style={{width:"50%"}}/></div>
+                </div>
+            </div>
+        </div>
+        <div className="my-achieve">
+            <h1>My Achievements</h1>
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>3+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>12+</h1>
+                <p>PROJECTS COMPLETED</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>7+</h1>
+                <p>CLIENTS</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
+
+
+
+
+
+
+
+
+
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/my.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt="My Profile"/>
+        <h1><span>I'm Abadanaf Chala,</span> Web Developer in Ethiopia</h1>
+        <p>I'm Software Engineering Student at Haramaya University</p>
+        <div className="hero-action">
+           <div className="hero-connect">Connect me</div> 
+           <div className="hero-resume">My resume</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
