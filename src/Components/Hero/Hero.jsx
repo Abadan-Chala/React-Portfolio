7f31e87d10c3fd7ba3ebfef41d40930354ce21ca
Abadanaf Chala <abadanafchala@gmail.com>
2024-12-08 05:47:52 +0300

    Update Hero.jsx paragraph added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,8 @@ const Hero = () => {
     <div id='home' className='hero'>
         <img src={profile_img} alt="My Profile"/>
         <h1><span>I'm Abadanaf Chala,</span> Web Developer in Ethiopia</h1>
-        <p>I'm Software Engineering Student at Haramaya University</p>
+        <p>I'm Software Engineering Student at Haramaya University.</p>
+        <p>Now, I'm working on frontend web developing as a freelancer</p>
         <div className="hero-action">
            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect me</AnchorLink></div> 
            <div className="hero-resume">My resume</div>
